9ad68d1983623bc29c429e2e30398e7a09a9bf4d
ZainsRockert <marcosjared02@gmail.com>
2024-05-18 22:51:47 -0400

    Prove 4 completed

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        string input = "";
+        int breathingLog = 0;
+        int reflectingLog = 0;
+        int listingLog = 0;
+
+        void DisplayLog()
+        {
+            Console.WriteLine("Activity Log - Current Session");
+            Console.WriteLine($"Breathing Activity: {breathingLog}");
+            Console.WriteLine($"Reflecting Activity: {reflectingLog}");
+            Console.WriteLine($"Listing Activity: {listingLog}");
+            Console.WriteLine();
+        }
+
+        while (input != "4")
+        {
+            Console.Clear();
+            DisplayLog();
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Start Breathing Activity");
+            Console.WriteLine("2. Start Reflecting Activity");
+            Console.WriteLine("3. Start Listing Activity");
+            Console.WriteLine("4. Exit Program");
+            Console.Write("Select a choice from the menu: ");
+
+            input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    BreathingActivity breathingActivity = new BreathingActivity();
+                    breathingActivity.RunActivity();
+                    breathingActivity.PromptBreathing();
+                    breathingActivity.EndActivity();
+
+                    breathingLog++;
+                    break;
+                
+                case "2":
+                    ReflectionActivity reflectionActivity = new ReflectionActivity();
+                    reflectionActivity.RunActivity();
+                    reflectionActivity.PromptReflection();
+                    reflectionActivity.EndActivity();
+
+                    reflectingLog++;
+                    break;
+
+                case "3":
+                    ListingActivity listingActivity = new ListingActivity();
+                    listingActivity.RunActivity();
+                    listingActivity.PromptListing();
+                    listingActivity.EndActivity();
+
+                    listingLog++;
+                    break;
+
+                case "4":
+                    Console.WriteLine("Thank you for using our program!");
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter the number of the menu option. ");
+                    break;
+            }
+        }
+        Environment.Exit(0);
     }
 }
+
+// I did some improve in the menu of the program
+// I added more prompt in the reflection activity
